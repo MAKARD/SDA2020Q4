54b7ef0cd2aa854f05282961f96d3426828b35cc
MAKARD01 <dmytro.makarenko@kingfisher.com>
2020-10-10 21:28:46 +0300

    Implement Item & ItemWeightComparator

--- src/Item.ts
+++ src/Item.ts
@@ -2,10 +2,46 @@ import { Comparable } from './Comparable';
 
 let id = 0;
 
-export abstract class Item implements Comparable<Item> {
-    public compareTo(other: Item): number {
-        // your code goes here
+const compareValues = (
+    leftValue: string | number,
+    rightValue: string | number
+): 0 | 1 | -1 => {
+    if (leftValue === rightValue) {
+        return 0;
     }
 
-    // your code goes here
+    return leftValue > rightValue ? 1 : -1;
+}
+
+export abstract class Item implements Comparable<Item> {
+    public static reset() {
+        id = 0;
+    }
+
+    public id: number;
+    public name: string;
+    public value: number;
+    public weight: number;
+
+    constructor(name: string, value: number, weight: number) {
+        this.name = name;
+        this.weight = weight;
+        this.value = value;
+
+        this.id = id;
+
+        id += 1;
+    }
+
+    public compareTo(other: Item) {
+        const result = compareValues(this.value, other.value);
+
+        return result
+            ? result
+            : compareValues(this.name.toLowerCase(), other.name.toLowerCase());
+    }
+
+    public toString() {
+        return `${this.name} - Value: ${this.value}, Weight: ${this.weight}`;
+    }
 }
--- src/ItemWeightComparator.ts
+++ src/ItemWeightComparator.ts
@@ -3,6 +3,10 @@ import { ItemComparator } from './ItemComparator';
 
 export class ItemWeightComparator implements ItemComparator {
     public compare(first: Item, second: Item) {
-        // your code goes here
+       if (first.weight === second.weight) {
+           return first.compareTo(second);
+       }
+
+       return first.weight > second.weight ? 1 : -1;
     }
 }
