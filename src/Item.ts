2aa3bfeecd5dcbbd23274edafbba89c85f0c7d36
MAKARD01 <dmytro.makarenko@kingfisher.com>
2020-10-10 22:18:44 +0300

    Implement Weapon

--- src/Item.ts
+++ src/Item.ts
@@ -3,45 +3,48 @@ import { Comparable } from './Comparable';
 let id = 0;
 
 const compareValues = (
-    leftValue: string | number,
-    rightValue: string | number
+  leftValue: string | number,
+  rightValue: string | number
 ): 0 | 1 | -1 => {
-    if (leftValue === rightValue) {
-        return 0;
-    }
+  if (leftValue === rightValue) {
+    return 0;
+  }
 
-    return leftValue > rightValue ? 1 : -1;
+  return leftValue > rightValue ? 1 : -1;
 }
 
 export abstract class Item implements Comparable<Item> {
-    public static reset() {
-        id = 0;
-    }
+  public static reset() {
+    id = 0;
+  }
 
-    public id: number;
-    public name: string;
-    public value: number;
-    public weight: number;
+  public id: number;
+  public name: string;
+  public value: number;
+  public weight: number;
 
-    constructor(name: string, value: number, weight: number) {
-        this.name = name;
-        this.weight = weight;
-        this.value = value;
+  constructor(name: string, value: number, weight: number) {
+    this.name = name;
+    this.weight = weight;
+    this.value = value;
 
-        this.id = id;
+    this.id = id;
 
-        id += 1;
-    }
+    id += 1;
+  }
 
-    public compareTo(other: Item) {
-        const result = compareValues(this.value, other.value);
+  public compareTo(other: Item) {
+    const result = compareValues(this.value, other.value);
 
-        return result
-            ? result
-            : compareValues(this.name.toLowerCase(), other.name.toLowerCase());
-    }
+    return result
+      ? result
+      : compareValues(this.name.toLowerCase(), other.name.toLowerCase());
+  }
 
-    public toString() {
-        return `${this.name} - Value: ${this.value}, Weight: ${this.weight}`;
-    }
+  public toString() {
+    const valueText = `Value: ${this.value}`;
+    const weightText = `Weight: ${this.weight.toFixed(2)}`;
+
+    return `${this.name} - ` + [valueText, weightText].join(', ').trim();
+  }
 }
--- src/Weapon.ts
+++ src/Weapon.ts
@@ -1,1 +1,81 @@
-// your code goes here
+import { Item } from './Item';
+
+export abstract class Weapon extends Item {
+  public static readonly MODIFIER_CHANGE_RATE = 0.05;
+
+  protected baseDamage: number;
+  protected damageModifier: number;
+  protected baseDurability: number;
+  protected durabilityModifier: number;
+
+  constructor(
+    name: string,
+    baseDamage: number,
+    baseDurability: number,
+    value: number,
+    weight: number
+  ) {
+    super(name, value, weight);
+
+    this.baseDamage = baseDamage;
+    this.setBaseDurability(baseDurability);
+  }
+
+  public setDamageModifier(modifier: number) {
+    this.damageModifier = modifier;
+  }
+
+  public setDurabilityModifier(modifier: number) {
+    this.durabilityModifier = modifier;
+  }
+
+  public getDamage() {
+    return this.baseDamage + this.damageModifier;
+  }
+
+  public getDurability() {
+    return this.baseDurability + this.durabilityModifier;
+  }
+
+  private setBaseDurability(durability: number) {
+    this.baseDurability = durability;
+  }
+
+  private getStatus(): 'operable' | 'breaking' | 'broken' {
+    const durability = this.getDurability();
+
+    if (durability > Weapon.MODIFIER_CHANGE_RATE) {
+      return 'operable';
+    }
+
+    return durability < 0 ? 'broken' : 'breaking';
+  }
+
+  public use() {
+    const operableText = `You use the ${this.name}, dealing ${this.getDamage().toFixed(2)} points of damage.`;
+    const breakingText = `\nThe ${this.name} breaks.`;
+    const brokenText = `You can't use the ${this.name}, it is broken.`;
+
+    const status = this.getStatus();
+    this.setBaseDurability(this.baseDurability - Weapon.MODIFIER_CHANGE_RATE);
+
+    switch(status) {
+      case 'operable': {
+       return operableText;
+      }
+      case 'breaking': {
+        return operableText + breakingText;
+      }
+      case 'broken': {
+        return brokenText;
+      }
+    }
+  }
+
+  public toString() {
+    const damageText = `Damage: ${this.getDamage().toFixed(2)}`;
+    const durabilityText = `Durability: ${(this.getDurability() * 100).toFixed(2)}%`;
+
+    return [super.toString(), damageText, durabilityText].join(', ').trim();
+  }
+}
