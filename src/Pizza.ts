1f7f80fcbcfa773e90a97d0c046e29442aab1564
MAKARD01 <dmytro.makarenko@kingfisher.com>
2020-10-10 23:56:32 +0300

    Implement Pizza

--- src/Pizza.ts
+++ src/Pizza.ts
@@ -1,1 +1,26 @@
-// your code goes here
+import { Consumable } from './Consumable';
+
+export class Pizza extends Consumable {
+  protected numberOfSlices: number;
+  protected slicesEaten = 0;
+
+  constructor(numberOfSlices: number, spoiled: boolean) {
+    super('pizza', 1, 1, spoiled);
+
+    this.numberOfSlices = numberOfSlices;
+  }
+
+  public eat() {
+    if (this.slicesEaten === this.numberOfSlices) {
+      return super.eat();
+    }
+
+    this.slicesEaten++;
+
+    if (this.slicesEaten >= this.numberOfSlices) {
+      this.setConsumed(true);
+    }
+
+    return `You eat a slice of the ${this.name}.`;
+  }
+}
