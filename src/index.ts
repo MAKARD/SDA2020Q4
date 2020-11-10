044aeecfdb9321d49daf151090eb3346dd318d8a
MAKARD01 <dmytro.makarenko@kingfisher.com>
2020-11-10 18:38:54 +0200

    Setup boilerplate

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root')!;
+
+  const input = document.createElement('input');
+  
+  root.appendChild(input);
+});
